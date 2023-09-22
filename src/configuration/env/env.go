08cd92213a40acf4629e06386be1053e08fe2219
lucasantoniodev <lucasantoniodev@gmail.com>
2023-09-22 13:20:13 -0300

    feat: error standardization

---
+++ src/configuration/env/env.go
@@ -0,0 +1,13 @@
+package configuration
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func LoadEnvSettings() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
